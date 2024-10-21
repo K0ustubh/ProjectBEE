e108c7b84a34e2a9dd0cd0311aced5b02fd320fd
K0ustubh <koustubhkukreti@outlook.com>
2024-10-21 11:44:51 +0530

    server

---
+++ server.js
@@ -0,0 +1,27 @@
+// const express = require('express');
+// const app = express();
+
+// // Define the port
+// const PORT = 3000;
+
+// // Create a basic route
+// app.get('/', (req, res) => {
+//   res.send('Hello, Express!');
+// });
+
+// // Server listens on the defined port
+// app.listen(PORT, () => {
+//   console.log(Server running at http://localhost:${PORT}/);
+// });
+
+
+// FRAMEWORK CONFIGURATION
+const express=require("express");
+const connectDb=require("./config/dbConnection");
+const errorHandler=require("./middleware/errorHandler");
+const cors=require("cors");
+connectDb();
+const app=express ();
+const port = process.env.PORT || 5000;
+app.use(express.json());
+app.use(cors());
