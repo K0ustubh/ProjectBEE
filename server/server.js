e4c23b602793b0514fe7dfc3f7e6b850ddadde29
K0ustubh <koustubhkukreti@outlook.com>
2024-10-23 11:17:47 +0530

    day 3

---
+++ server/server.js
@@ -0,0 +1,62 @@
+// const express = require('express');
+// const app = express();
+
+// // Define the port
+// const PORT = 3000;
+
+// // Create a basic route
+// app.get('/', (req, res) => {
+//   res.send('Hello, Express!');
+// });
+
+// // Server listens on the defined port
+// app.listen(PORT, () => {
+//   console.log(Server running at http://localhost:${PORT}/);o
+// });
+
+
+// FRAMEWORK CONFIGURATION
+
+const express = require("express");
+const connectDb = require("./config/dbConnection"); // Ensure this path is correct
+//const errorHandler = require("./middleware/errorHandler"); // Ensure this path is correct
+const cors = require("cors");
+
+// Connect to the database
+
+
+// Create an Express application
+
+// Middleware
+// app.use(express.json());
+// app.use(cors());
+
+// Basic route
+// app.get("/", (req, res) => {
+//     res.send("Hello World");
+// });
+
+// Error handling middleware
+// app.use(errorHandler);
+
+// Start the server
+// app.listen(port, () => {
+//     console.log(Server is running on port ${port});
+// });
+
+// env file config
+const dotenv=require("dotenv");
+dotenv.config();
+connectDb();
+const app = express();
+app.set('view engine','hbs');
+app.get("/home",(req,res)=>{
+    res.render("home",{
+        
+    })
+})
+const port = process.env.PORT || 5001;
+
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
+});
