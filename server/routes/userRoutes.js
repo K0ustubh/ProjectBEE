b5cac0294cb3c127822804264dc2bf334e7e375a
K0ustubh <koustubhkukreti@outlook.com>
2024-11-25 10:47:03 +0530

    error fix

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -10,7 +10,7 @@ router.post("/register", registerUser);
 
 // Route to login a user
 router.post("/login",loginUser);
-router.get("/myAccount",validateJwtToken,myAccount);
+// router.get("/myAccount",validateJwtToken,myAccount);
 //router.put('/myAccount', validateJwtToken, myAccount);
 router
 
